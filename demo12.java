75053d3bd19ea943f9e14b711901db256a79e890
mdehsan <mdehsan480@gmail.com>
2023-01-25 16:07:24 +0530

    this is change by dev1

--- demo12.java
+++ demo12.java
@@ -1,6 +1,8 @@
-class demo12{
-Public Static void main(String args[])
+class demo12 {
+public static void main(String args[])
 {
+System.out.println("this is md ehsan");
+System.out.println("this is md ehsan");
 System.out.println("this is md ehsan");
 }
 }
